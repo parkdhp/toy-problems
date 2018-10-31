cf51fbb3ed069141eb300e77f64c02c6b470d787
Peter Park <parkdhp@gmail.com>
2018-10-31 03:39:40 -0400

    [added]class Card

--- problems/objectOrientedDesign/deckOfCards.js
+++ problems/objectOrientedDesign/deckOfCards.js
@@ -3,3 +3,10 @@
  * would subclass the data structures to implement blackjack.
  */
 
+class Card {
+  constructor(suit, number) {
+    this.suit = suit;
+    this.number = number;
+    this.value = `${this.number}${this.suit}`;
+  }
+}
