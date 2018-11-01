e50a951cb13b19449572e424efb2148c6a1fde13
Peter Park <parkdhp@gmail.com>
2018-11-01 03:52:34 -0400

    [added] Deck with methods clear, shuffle, and deal

--- problems/objectOrientedDesign/deckOfCards.js
+++ problems/objectOrientedDesign/deckOfCards.js
@@ -10,3 +10,38 @@ class Card {
     this.value = `${this.number}${this.suit}`;
   }
 }
+
+class Deck {
+  constructor() {
+    this.cards = [];
+    this.newDeck();
+  }
+  newDeck() {
+    this.clear();
+    let suits = ['♥', '♣', '♠', '♦'],
+      numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    suits.forEach((suit) => {
+      numbers.forEach((number) => {
+        this.cards.push(new Card(suit, number));
+      });
+    });
+  }
+  clear() {
+    this.cards = [];
+  }
+  shuffle() {
+    const swap = (firstIndex, secondIndex, cards) => {
+      let temp = cards[firstIndex];
+      cards[firstIndex] = cards[secondIndex];
+      cards[secondIndex] = temp;
+    }
+    for (let i = 0; i < this.cards.length; i++) {
+      let randomIndex = i + Math.floor(Math.random() * (this.cards.length - i));
+      swap(i, randomIndex, this.cards);
+    }
+  }
+  deal() {
+    this.deck = new Deck();
+    this.hand = [];
+  }
+}
