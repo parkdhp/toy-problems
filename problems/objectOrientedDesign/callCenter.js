b360180c5c63a8d38ba71986918ece5f6365c043
Peter Park <parkdhp@gmail.com>
2018-11-02 22:41:17 -0400

    [added] class CallCenter

--- problems/objectOrientedDesign/callCenter.js
+++ problems/objectOrientedDesign/callCenter.js
@@ -27,3 +27,47 @@ class Call {
     this.time = time;
   }
 }
+
+class CallCenter {
+  constructor() {
+    this.respondentQ = new Queue;
+    this.managerQ = new Queue;
+    this.directorQ = new Queue;
+    this.open = false;
+    this.init = false;
+  }
+  start() {
+    if (this.init) {
+      console.log(`already initialized`);
+      return;
+    }
+    for (let i = 0; i < 3; i++) {
+      this.respondentQ.enqueue(new Employee(`respondent${i}`));
+      this.respondentQ.enqueue(new Employee(`manager${i}`));
+      this.respondentQ.enqueue(new Employee(`director${i}`));
+    }
+    this.init = true;
+  }
+  dispatchCall(call) {
+    let employee;
+    if(!this.respondentQ.isEmpty()) {
+      employee = this.respondentQ.dequeue();
+      console.log(employee, 'will be deployed');
+      employee.dispatch(call, this.respondentQ);
+      console.log('A respondent will be taking your call!');
+    } else if (!this.managerQ.isEmpty()) {
+      employee = this.managerQ.dequeue();
+      console.log(employee, 'will be deployed');
+      employee.dispatch(call, this.respondentQ);
+      console.log('A manager will be taking your call!');
+    } else if (!this.directorQ.isEmpty()) {
+      employee = this.directorQ.dequeue();
+      console.log(employee, 'will be deployed');
+      employee.dispatch(call, this.respondentQ);
+      console.log('A director will be taking your call!');
+    } else {
+      console.error('sorry, there are currently no available staff to take your call')
+    }
+  }
+}
+
