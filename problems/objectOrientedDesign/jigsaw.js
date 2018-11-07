6061fde9fed141137fd0b2bb79ddc593cd8e8fb2
Peter Park <parkdhp@gmail.com>
2018-11-07 11:17:47 -0500

    init

---
+++ problems/objectOrientedDesign/jigsaw.js
@@ -0,0 +1,5 @@
+/**Implement an NxN jigsaw puzzle. Design the data structures and explain an algorithm
+ * to solve the puzzle. You can assume that you have a fitsWith method which, when pasted
+ * two puzzle edges, returns true if the two edges belong together.
+ */
+
