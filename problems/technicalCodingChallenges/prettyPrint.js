009e7812a33b5f88a9309143d6b2c57541e8ffc9
Peter Park <parkdhp@gmail.com>
2018-12-04 22:17:27 -0500

    removed JSON.stringify to recursively pretty print object

--- problems/technicalCodingChallenges/prettyPrint.js
+++ problems/technicalCodingChallenges/prettyPrint.js
@@ -1,6 +1,37 @@
+const isEmpty = obj => {
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      return false;
+    }
+  }
+  return true;
+};
+
 function prettyPrint(data) {
-  let result = JSON.stringify(data, null, 2)
-  return result;
+  let output = '{\n';
+
+  const recurse = (obj, indent) => {
+    let result = '';
+    indent = indent || '';
+    for (const key in obj) {
+      let value = obj[key];
+      if (typeof value === 'string') {
+        value = `"${value}"\n`;
+      } else if (typeof value === 'object') {
+        if (isEmpty(value)) {
+          value = `{}`
+        } else {
+          const innerObj = recurse(value, `${indent}  `);
+          value = `\n${indent}{${innerObj}\n${indent}}`;
+        }
+      }
+      result += `${indent}"${key}": ${value}`;
+    }
+    return result.replace(/,\n$/, '');
+  };
+  
+  output += `${recurse(data, '  ')}\n}`;
+  return output;
 }
 
 const exampleJson = {
@@ -8,7 +39,7 @@ const exampleJson = {
   facts: {
     car: 'Ford',
     address: {
-      city: 'New York'
+      city: 'New York',
     },
     watch: 'Casio',
     other: {},
@@ -27,4 +58,5 @@ const expectedResult = `{
   }
 }`;
 
+console.log(prettyPrint(exampleJson));
 console.log(expectedResult === prettyPrint(exampleJson));
